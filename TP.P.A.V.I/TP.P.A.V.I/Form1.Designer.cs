6b0bfe730cfe27cf27a5c375d16fa01f4ac0c36b
70251-Facundo-Romero <facurome26@gmail.com>
2021-04-19 13:50:30 -0300

    Agrege un icono propio para la app en la barra de tareas
    No pude hacer que se maximice el cliente sin ocultar la barra de tareas, encontré una solución muy casera, que sirve pero hasta ahí, si alguien se anima a buscar otra solución bienvenido sea

--- TP.P.A.V.I/TP.P.A.V.I/Form1.Designer.cs
+++ TP.P.A.V.I/TP.P.A.V.I/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.label1 = new System.Windows.Forms.Label();
             this.TxtPassword = new System.Windows.Forms.TextBox();
             this.TxtUserName = new System.Windows.Forms.TextBox();
@@ -293,6 +294,7 @@
             this.Controls.Add(this.MenuLogin);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Login";
--- TP.P.A.V.I/TP.P.A.V.I/Menu.Designer.cs
+++ TP.P.A.V.I/TP.P.A.V.I/Menu.Designer.cs
@@ -185,10 +185,13 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1000, 500);
+            this.ControlBox = false;
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.MenuLogin);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Menu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Menu";
