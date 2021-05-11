6960b64bb92fd0fc0543c522c7d65db9935cbd54
74297-ContiGraffignaMarcelo <lutycontig@gmail.com>
2021-05-11 13:42:21 -0300

    ABM EMPLEADOS Y HOTELES

---
+++ TP.P.A.V.I/TP.P.A.V.I/DAL/HotelesDAL.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP.P.A.V.I.Entities;
+
+
+namespace TP.P.A.V.I.DAL
+{
+    class HotelesDAL : EntidadDAL
+    {
+
+        public static DataTable ObtenerListadoHoteles()
+        {
+            string cadenaConexion = ConfigurationManager.ConnectionStrings["ConexionBD"].ConnectionString;
+            SqlCommand cmd = new SqlCommand();
+            SqlConnection cn = new SqlConnection(cadenaConexion);
+
+            try
+            {
+                string consulta = "SELECT * FROM Hoteles";
+
+                cmd.Parameters.Clear();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = consulta;
+
+                cn.Open();
+                cmd.Connection = cn;
+
+                DataTable tabla = new DataTable();
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(tabla);
+                return tabla;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+
+        }
+
+        public static bool AgregarHotelABD(Hotel h)
+        {
+            string cadenaConexion = ConfigurationManager.ConnectionStrings["ConexionBD"].ConnectionString;
+            bool resultado = false;
+            SqlCommand cmd = new SqlCommand();
+            SqlConnection cn = new SqlConnection(cadenaConexion);
+
+            try
+            {
+                string consulta = "INSERT INTO Hoteles (Nombre, Calle, NumeroCalle, Id_Barrio) VALUES (@nombre, @calle,@numCalle,@idBarrio)";
+
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@nombre", h.NombreHotel);
+                cmd.Parameters.AddWithValue("@calle", h.CalleHotel);
+                cmd.Parameters.AddWithValue("@numCalle", h.NumeroCalle);
+                cmd.Parameters.AddWithValue("@idBarrio", h.IdBarrio);
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = consulta;
+
+                cn.Open();
+                cmd.Connection = cn;
+                cmd.ExecuteNonQuery();
+                resultado = true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+            return resultado;
+        }
+
+        public static Hotel ObtenerHotel(int id)
+        {
+            string cadenaConexion = ConfigurationManager.ConnectionStrings["ConexionBD"].ConnectionString;
+            Hotel h = new Hotel();
+            SqlCommand cmd = new SqlCommand();
+            SqlConnection cn = new SqlConnection(cadenaConexion);
+
+            try
+            {
+                string consulta = "SELECT * FROM Hoteles WHERE Id LIKE @id";
+
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@id", id);
+
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = consulta;
+
+                cn.Open();
+                cmd.Connection = cn;
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr != null && dr.Read())
+                {
+                    h.IdHotel = int.Parse(dr["Id"].ToString());
+                    h.NombreHotel = dr["Nombre"].ToString();
+                    h.CalleHotel = dr["Calle"].ToString();
+                    h.NumeroCalle = dr["NumeroCalle"].ToString();
+                    h.IdBarrio = int.Parse(dr["Id_Barrio"].ToString());
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+            return h;
+        }
+
+        public static bool ActualizarHotelABD(Hotel h)
+        {
+            string cadenaConexion = ConfigurationManager.ConnectionStrings["ConexionBD"].ConnectionString;
+            bool resultado = false;
+            SqlCommand cmd = new SqlCommand();
+            SqlConnection cn = new SqlConnection(cadenaConexion);
+
+            try
+            {
+                string consulta = "UPDATE Hoteles SET Nombre = @nombre, Calle = @calle, NumeroCalle=@numCalle,Id_Barrio=@idBarrio WHERE Id LIKE @id";
+
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@id", h.IdHotel);
+                cmd.Parameters.AddWithValue("@nombre", h.NombreHotel);
+                cmd.Parameters.AddWithValue("@numCalle", h.NumeroCalle);
+                cmd.Parameters.AddWithValue("@calle", h.CalleHotel);
+                cmd.Parameters.AddWithValue("@idBarrio", h.IdBarrio);
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = consulta;
+
+                cn.Open();
+                cmd.Connection = cn;
+                cmd.ExecuteNonQuery();
+                resultado = true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+            return resultado;
+        }
+
+        public static bool BorrarHotelABD(Hotel h)
+        {
+            string cadenaConexion = ConfigurationManager.ConnectionStrings["ConexionBD"].ConnectionString;
+            bool resultado = false;
+            SqlCommand cmd = new SqlCommand();
+            SqlConnection cn = new SqlConnection(cadenaConexion);
+
+            try
+            {
+                string consulta = "DELETE FROM Hoteles WHERE Id LIKE @id";
+
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@id", h.IdHotel);
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = consulta;
+
+                cn.Open();
+                cmd.Connection = cn;
+                cmd.ExecuteNonQuery();
+                resultado = true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+            return resultado;
+        }
+        public static DataTable ObtenerListadoBarrios()
+        {
+            
+            string cadenaConexion = ConfigurationManager.ConnectionStrings["ConexionBD"].ConnectionString;
+            SqlConnection cn = new SqlConnection(cadenaConexion);
+            try
+            {
+
+
+                SqlCommand cmd = new SqlCommand();
+
+
+                string consulta = "SELECT * FROM Barrios";
+                cmd.Parameters.Clear();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = consulta;
+
+                cn.Open();
+                cmd.Connection = cn;
+                DataTable tabla = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(tabla);
+
+
+                return tabla;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+            
+        }
+
+
+    }
+}
--- TP.P.A.V.I/TP.P.A.V.I/Menu.cs
+++ TP.P.A.V.I/TP.P.A.V.I/Menu.cs
@@ -16,6 +16,36 @@ namespace TP.P.A.V.I
         public Menu()
         {
             InitializeComponent();
+            customizeDesing();
+        }
+
+        private void customizeDesing()
+        {
+            subMenuHoteles.Visible = false;
+            subMenuHuespedes.Visible = false;
+        }
+
+        private void hideSubMenu()
+        {
+            if (subMenuHoteles.Visible == true) subMenuHoteles.Visible = false;
+            if (subMenuHuespedes.Visible == true) subMenuHuespedes.Visible = false;
+            if (subMenuUbicaciones.Visible == true) subMenuUbicaciones.Visible = false;
+            if (subMenuServIns.Visible == true) subMenuServIns.Visible = false;
+            if (subMenuFacturacion.Visible == true) subMenuFacturacion.Visible = false;
+            if (subMenuEmpleados.Visible == true) subMenuEmpleados.Visible = false;
+        }
+
+        private void showSubMenu(Panel subMenu)
+        {
+            if (subMenu.Visible == false)
+            {
+                hideSubMenu();
+                subMenu.Visible = true;
+            }
+            else
+            {
+                subMenu.Visible = false;
+            }
         }
 
         private void Menu_Load(object sender, EventArgs e)
@@ -69,5 +99,60 @@ namespace TP.P.A.V.I
             ReleaseCapture();
             SendMessage(this.Handle, 0x112, 0xf012, 0);
         }
-    }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            showSubMenu(subMenuHoteles);
+        }
+
+        private void btnHuespedes_Click(object sender, EventArgs e)
+        {
+            showSubMenu(subMenuHuespedes);
+        }
+
+        private void btnEmpleados_Click(object sender, EventArgs e)
+        {
+            showSubMenu(subMenuEmpleados);
+        }
+
+        private Form activeForm = null;
+        private void openChildForm(Form formHijo)
+        {
+            if (activeForm != null)
+                activeForm.Close();
+            activeForm = formHijo;
+            formHijo.TopLevel = false;
+            formHijo.FormBorderStyle = FormBorderStyle.None;
+            formHijo.Dock = DockStyle.Fill;
+            panelFormulariosHijos.Controls.Add(formHijo);
+            panelFormulariosHijos.Tag = formHijo;
+            formHijo.BringToFront();
+            formHijo.Show();
+        }
+
+        private void btnUbicaciones_Click(object sender, EventArgs e)
+        {
+            showSubMenu(subMenuUbicaciones);
+        }
+
+        private void btnServIns_Click(object sender, EventArgs e)
+        {
+            showSubMenu(subMenuServIns);
+        }
+
+        private void btnFacturacion_Click(object sender, EventArgs e)
+        {
+            showSubMenu(subMenuFacturacion);
+        }
+
+        private void btnGestionHoteles_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGestEmpleados_Click(object sender, EventArgs e)
+        {
+
+        }
+    }   
 }
