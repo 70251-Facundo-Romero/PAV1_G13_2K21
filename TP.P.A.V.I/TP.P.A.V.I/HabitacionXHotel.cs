4c9896787c6bbae876b3d504c682d959c7f1118f
Nataniel <gigena877@gmail.com>
2021-05-11 19:15:18 -0300

    HXH 2

---
+++ TP.P.A.V.I/TP.P.A.V.I/HabitacionXHotel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP.P.A.V.I
+{
+    public partial class HabitacionXHotel : Form
+    {
+        public HabitacionXHotel()
+        {
+            InitializeComponent();
+        }
+
+        //private void CargarHotel()
+        //{
+        //    CmbHotel.DataSource = PersonaBLL.SelectAllTipoDoc();
+        //    CmbHotel.DisplayMember = "Nombre";
+        //    CmbHotel.ValueMember = "Id";
+        //    CmbHotel.SelectedIndex = -1;
+        //}
+
+        //private void CargarCarreras()
+        //{
+        //    CmbHabitacion.DataSource = PersonaBLL.SelectAllCarreras();
+        //    CmbHabitacion.DisplayMember = "Nombre";
+        //    CmbHabitacion.ValueMember = "Id";
+        //    CmbHabitacion.SelectedIndex = -1;
+
+        //}
+    }
+}
